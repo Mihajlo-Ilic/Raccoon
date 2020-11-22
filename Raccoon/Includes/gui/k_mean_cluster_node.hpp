882991e3ffb99c754c39b429acc2583877567206
Gavrilo Jovanovic <jovanovic.nemanjic@gmail.com>
2020-11-22 14:54:20 +0000

    Upload New File

---
+++ Raccoon/Includes/gui/k_mean_cluster_node.hpp
@@ -0,0 +1,34 @@
+
+#ifndef K_MEAN_CLUSTER_NODE_HPP
+#define K_MEAN_CLUSTER_NODE_HPP
+
+#include "node.hpp"
+#include "../k_mean_cluster.hpp"
+#include <QLineEdit>
+#include <QPushButton>
+#include <QTableWidget>
+#include <QSpinBox>
+#include <QLabel>
+#include <QComboBox>
+
+class k_mean_cluster_node : public node
+{
+    Q_OBJECT
+private:
+    QComboBox distance_box;
+    QPushButton previewBtn;
+    QSpinBox k_neighbours;
+    QLabel distance_label;
+    QLabel k_neighbours_label;
+    k_mean_cluster model;
+public:
+    k_mean_cluster_node(int width, int height);
+    void run() override;
+    void on_input_changed() override;
+public slots:
+    void preview_b();
+    void combo_changed(QString str);
+    void sb_changed(int val);
+};
+
+#endif
