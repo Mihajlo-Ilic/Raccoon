a8c88d7edbb816f597d6afe453a80d2e2982db84
Gavrilo Jovanovic <jovanovic.nemanjic@gmail.com>
2020-11-22 14:53:10 +0000

    Upload New File

---
+++ Raccoon/Includes/gui/aproximation_node.hpp
@@ -0,0 +1,32 @@
+#ifndef APROXIMATION_NODE_HPP
+#define APROXIMATION_NODE_HPP
+
+#include "node.hpp"
+#include <QComboBox>
+#include <QLabel>
+#include <QListWidget>
+#include <QPushButton>
+#include <QObject>
+#include <QLineEdit>
+
+class aproximation_node : public node
+{
+    Q_OBJECT
+public:
+    aproximation_node(int width,int height);
+    void on_input_changed() override;
+    void run() override;
+private:
+    QComboBox aproximationCombo;
+    QLabel aproximationLabel;
+    QListWidget columns;
+    QPushButton previewBtn;
+    QLineEdit editAprox;
+public slots:
+    void preview_b();
+    void changed();
+    void help_func(const QString &text);
+
+};
+
+#endif // APROXIMATION_NODE_HPP
