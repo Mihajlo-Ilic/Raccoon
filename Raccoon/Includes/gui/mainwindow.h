857fa5aa72d195f38636f4dc8e5be32b7c4e8d74
Mihajlo Ilic <herb.m90@gmail.com>
2020-11-22 16:04:56 +0000

    Upload New File

---
+++ Raccoon/Includes/gui/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <vector>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    virtual void keyPressEvent(QKeyEvent *event);
+
+    virtual void dragEnterEvent(QDragEnterEvent *event);
+    virtual void dragMoveEvent(QDragMoveEvent *event);
+    virtual void dropEvent(QDropEvent *event);
+
+public slots:
+    void drop_action();
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
