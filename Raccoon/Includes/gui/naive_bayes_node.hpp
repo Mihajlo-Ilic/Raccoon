3d3c05da72f6b429b154464dc003cbb7dfade2ce
Mihajlo Ilic <herb.m90@gmail.com>
2020-11-22 16:05:42 +0000

    Upload New File

---
+++ Raccoon/Includes/gui/naive_bayes_node.hpp
@@ -0,0 +1,34 @@
+#ifndef NB_NODE_HPP
+#define NB_NODE_HPP
+
+
+#include "node.hpp"
+#include "../naive_bayes.hpp"
+#include <QLineEdit>
+#include <QPushButton>
+#include <QTableWidget>
+#include <QSpinBox>
+#include <QLabel>
+#include <QCheckBox>
+
+class nb_node : public node
+{
+    Q_OBJECT
+private:
+    QPushButton previewBtn;
+    QDoubleSpinBox alpha_sbox;
+    QLabel alpha_label;
+    QCheckBox table_checkbox;
+    QLabel table_label;
+
+    naive_bayes classifier;
+public:
+    nb_node(int width, int height);
+    void run() override;
+    void on_input_changed() override;
+public slots:
+    void preview_b();
+};
+
+#endif
+
