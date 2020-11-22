faec653f0ddd777ff21b9556f8eb09d1a977a9ac
Gavrilo Jovanovic <jovanovic.nemanjic@gmail.com>
2020-11-22 15:08:11 +0000

    Upload New File

---
+++ Raccoon/Includes/gui/partition_node.hpp
@@ -0,0 +1,26 @@
+#ifndef PARTITION_NODE_HPP
+#define PARTITION_NODE_HPP
+#include "node.hpp"
+#include <QSpinBox>
+#include <QPushButton>
+
+class partition_node : public node
+{
+    Q_OBJECT
+public:
+    partition_node(int width, int height);
+    void on_input_changed() override;
+    void run() override;
+private:
+    QSpinBox sb_percent;
+    QSpinBox sb_random;
+    QPushButton preview_btn;
+    QLabel lbl_percent;
+    QLabel lbl_random;
+
+public slots:
+    void preview_b();
+    void changed(int i);
+};
+
+#endif // PARTITION_NODE_HPP
