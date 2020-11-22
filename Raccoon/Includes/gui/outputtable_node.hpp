cb9eeff43953476971daa6dd30be94e4334f57df
Gavrilo Jovanovic <jovanovic.nemanjic@gmail.com>
2020-11-22 14:55:38 +0000

    Upload New File

---
+++ Raccoon/Includes/gui/outputtable_node.hpp
@@ -0,0 +1,21 @@
+#ifndef OUTPUTTABLE_NODE_HPP
+#define OUTPUTTABLE_NODE_HPP
+
+#include "node.hpp"
+#include <QPushButton>
+#include <QObject>
+
+class outputTable_node : public node
+{
+    Q_OBJECT
+public:
+    outputTable_node(int width,int height);
+    void on_input_changed() override;
+    void run() override;
+private:
+    QPushButton previewBtn;
+public slots:
+    void preview_b();
+};
+
+#endif // OUTPUTTABLE_NODE_HPP
