78fca3206853ff858255b84559c14c6215bdfb16
Gavrilo Jovanovic <jovanovic.nemanjic@gmail.com>
2020-11-22 14:53:28 +0000

    Upload New File

---
+++ Raccoon/Includes/gui/binning_node.hpp
@@ -0,0 +1,32 @@
+#ifndef BINNING_NODE_HPP
+#define BINNING_NODE_HPP
+
+#include "node.hpp"
+#include <QComboBox>
+#include <QLabel>
+#include <QListWidget>
+#include <QPushButton>
+#include <QObject>
+
+class binning_node : public node
+{
+    Q_OBJECT
+private:
+    QListWidget listWidget;
+    QComboBox binningMethod;
+    QComboBox binningShuffle;
+    QLabel binningMethodLabel;
+    QLabel binningShuffleLabel;
+    QPushButton previewBtn;
+public:
+    void on_input_changed();
+    binning_node(int width, int height);
+    void run() override;
+public slots:
+    void changed(const QString& s);
+    void preview_b();
+    void list_changed();
+
+};
+
+#endif // BINNING_NODE_HPP
